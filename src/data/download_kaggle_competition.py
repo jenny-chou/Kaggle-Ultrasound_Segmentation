9d3bd7c572fa0541dd60000e67443b2505c046c9
jenny-chou <jennychou0823@gmail.com>
2021-01-17 15:37:49 -0800

    Remove unused files
    It's not a complicated project, lots of files are unnecessary.

--- src/data/download_kaggle_competition.py
+++ src/data/download_kaggle_competition.py
@@ -2,18 +2,19 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-
+import kaggle
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.option('--competition', prompt='Enter Kaggle competition name')
+@click.option('--output_filepath', default='../../data/raw')
+def main(competition):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    kaggle.api.authenticate()
+    kaggle.api.competition_download_files(competition, path=output_filepath)
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Downloaded dataset for Kaggle competition: %s to %s', competition, output_filepath)
 
 
 if __name__ == '__main__':
@@ -22,9 +23,5 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
