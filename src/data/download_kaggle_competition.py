4d8e50471e6527c19018413eb11a8a86f77b8799
jenny-chou <jennychou0823@gmail.com>
2021-01-17 19:53:59 -0800

    Turn script to stand alone helper function

--- src/data/download_kaggle_competition.py
+++ src/data/download_kaggle_competition.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
+import os
 import kaggle
 
-@click.command()
-@click.option('--competition', prompt='Enter Kaggle competition name: ')
-@click.option('--output_filepath', default='../../data/raw')
-def main(competition):
+def download_dataset(competition='ultrasound-nerve-segmentation', 
+                     output_filepath=os.path.join('..', '..', 'data', 'raw')):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     kaggle.api.authenticate()
     kaggle.api.competition_download_files(competition, path=output_filepath)
-    logger = logging.getLogger(__name__)
-    logger.info('Downloaded dataset for Kaggle competition: %s to %s', competition, output_filepath)
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    main()
